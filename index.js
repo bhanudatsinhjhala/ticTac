36a46f50e9caffc24838f957fba356da2e78b981
Bhanudatsinh Jhala <jhalabhanudattsinh4090@gmail.com>
2023-03-09 12:05:30 +0530

    Robot can stop user to win

--- index.js
+++ index.js
@@ -7,18 +7,29 @@ function emptyBoard() {
     ["-", "-", "-"],
   ];
 }
+let winningPositions = [
+  ["00", "01", "02"],
+  ["10", "11", "12"],
+  ["20", "21", "22"],
+  ["00", "10", "20"],
+  ["01", "11", "21"],
+  ["02", "12", "22"],
+  ["00", "11", "22"],
+  ["02", "11", "20"],
+];
 let players = [
   {
     name: "player1",
     value: "x",
+    positionsOccupied: [],
   },
   {
-    name: "player2",
+    name: "robot",
     value: "o",
   },
 ];
 let player1 = document.querySelector(".player1");
-let player2 = document.querySelector(".player2");
+// let player2 = document.querySelector(".player2");
 let playersForm = document.querySelector(".players-form");
 let boardForm = document.querySelector(".board");
 let heading = document.querySelector(".heading");
@@ -26,23 +37,25 @@ let cells = document.querySelectorAll(".cells");
 
 let playerTurnValue = "x";
 
+/* User Input Form for player name and board form input Code */
+
 function resetPlayersName() {
   player1.value = "";
-  player2.value = "";
+  // player2.value = "";
 }
 function getPlayersName() {
-  if (!player1.value || !player2.value) {
+  if (!player1.value) {
     alert("Please do not leave fields empty");
     resetPlayersName();
     return;
   }
-  if (player1.value.toLowerCase() === player2.value.toLowerCase()) {
-    alert("Please do not enter duplicate player names");
-    resetPlayersName();
-    return;
-  }
+  // if (player1.value.toLowerCase() === player2.value.toLowerCase()) {
+  //   alert("Please do not enter duplicate player names");
+  //   resetPlayersName();
+  //   return;
+  // }
   players[0].name = player1.value.toLowerCase();
-  players[1].name = player2.value.toLowerCase();
+  // players[1].name = player2.value.toLowerCase();
   playersForm.style.display = "none";
   boardForm.style.display = "block";
   displayPlayersTurn(0);
@@ -56,6 +69,7 @@ function btnClick(indexVal) {
   cell.setAttribute("disabled", true);
   board[rowIndex][columnIndex] = playerTurnValue;
   if (isPlayerWin(rowIndex, columnIndex, playerTurnValue)) {
+    console.log("is Player win ---", "yes");
     declareWin(playerTurnValue);
     return;
   }
@@ -66,19 +80,23 @@ function btnClick(indexVal) {
     }, 3000);
     return;
   }
+  if (playerTurnValue === "x") players[0].positionsOccupied.push(indexVal);
   playerTurnValue = players[0].value === playerTurnValue ? "o" : "x";
-  displayPlayersTurn(
-    players.findIndex((player) => player.value === playerTurnValue)
+  let nextPlayerIndex = players.findIndex(
+    (player) => player.value === playerTurnValue
   );
+  displayPlayersTurn(nextPlayerIndex, rowIndex, columnIndex);
 }
 
-function displayPlayersTurn(index) {
-  heading.innerText = `${players[index].name} place your ${players[index].value}`;
+function displayPlayersTurn(nextPlayerIndex) {
+  heading.innerText = `${players[nextPlayerIndex].name} place your ${players[nextPlayerIndex].value}`;
+  if (nextPlayerIndex === 1) return robotPlay();
 }
+
+/* Checking Is Player Won and declare it Code. */
 
 function isPlayerWin(rowIndex, columnIndex, playerTurnValue) {
-  if (checkRow(rowIndex, playerTurnValue)) return true;
-  else if (checkColumn(columnIndex, playerTurnValue)) return true;
+  if (checkRowAndColumn(rowIndex, columnIndex, playerTurnValue)) return true;
   else if (rowIndex !== columnIndex) return checkRightToLeft();
   else if (rowIndex === columnIndex && rowIndex === 1)
     return checkLeftToRight() || checkRightToLeft();
@@ -86,8 +104,11 @@ function isPlayerWin(rowIndex, columnIndex, playerTurnValue) {
   else return false;
 }
 
-function checkRow(rowIndex, playerTurnValue) {
-  return board[rowIndex].every((cellVal) => cellVal === playerTurnValue);
+function checkRowAndColumn(rowIndex, columnIndex, playerTurnValue) {
+  return (
+    board[rowIndex].every((cellVal) => cellVal === playerTurnValue) ||
+    board.every((row) => row[columnIndex] === playerTurnValue)
+  );
 }
 
 function checkLeftToRight() {
@@ -106,27 +127,116 @@ function checkRightToLeft() {
   if (count === 3) return true;
 }
 
-function checkColumn(columnIndex, playerTurnValue) {
-  return board.every((row) => row[columnIndex] === playerTurnValue);
-}
 function declareWin(playerTurnValue) {
   let index = players.findIndex((player) => player.value === playerTurnValue);
   heading.innerText = `Hurray ${players[index].name} wins`;
   modifyCellsAttribute(true);
 }
 
+/* Robot and User Board checking */
+
+function robotPlay() {
+  let tacklePosition = checkPlayerWinningPosition();
+  console.log(
+    "🚀 ~line number file: index.js:140 ~line number robotPlay ~line number tacklePosition:",
+    tacklePosition
+  );
+  let positionsOccupied = players[0].positionsOccupied;
+  console.log(
+    "🚀 ~line number file: index.js:145 ~line number robotPlay ~line number positionsOccupied:",
+    positionsOccupied
+  );
+  if (tacklePosition.length !== 0) {
+    putRobotsValue(tacklePosition, positionsOccupied);
+  } else {
+    let rowIndex = Math.round(0 + Math.random() * (2 - 0));
+    let columnIndex = Math.round(0 + Math.random() * (2 - 0));
+    console.log(rowIndex, columnIndex);
+    while (board[rowIndex][columnIndex] !== "-") {
+      rowIndex = Math.round(0 + Math.random() * (2 - 0));
+      columnIndex = Math.round(0 + Math.random() * (2 - 0));
+      console.log("while works -----", rowIndex, columnIndex);
+    }
+    btnClick(`${rowIndex}${columnIndex}`);
+  }
+}
+
+function putRobotsValue(tacklePosition, positionsOccupied) {
+  let indexVal;
+  indexVal = tacklePosition[0].filter((value) => {
+    // console.log(
+    //   "🚀 ~line number file: index.js:151 ~line number robotPlay ~line number value:",
+    //   value
+    // );
+    // console.log(
+    //   "find--------",
+    //   tacklePosition.find((item) => item === value)
+    // );
+    let resultIndex = positionsOccupied.includes(value);
+    // console.log(
+    //   "🚀 ~line number file: index.js:161 ~line number indexVal ~line number resultIndex:",
+    //   resultIndex
+    // );
+    if (!resultIndex) {
+      return value;
+    }
+  });
+  console.log(
+    "🚀 ~line number file: index.js:168 ~line number indexVal ~line number indexVal:",
+    indexVal
+  );
+  let rowIndex = indexVal[0].split("")[0];
+  let columnIndex = indexVal[0].split("")[1];
+  if (board[rowIndex][columnIndex]) {
+    btnClick(`${indexVal}`);
+  } else {
+    putRobotsValue();
+  }
+}
+
+function checkPlayerWinningPosition() {
+  let positionsOccupied = players[0].positionsOccupied;
+  console.log("positionsOccupied----", positionsOccupied);
+  let playerWinningPositions = winningPositions.filter((positions) => {
+    let count = 0;
+    console.log("positions----", positions);
+    positions.forEach((position) => {
+      console.log("particular position ----", position);
+      let isUsersVal =
+        positionsOccupied.find((value) => value === position) !== undefined;
+      // console.log("isUsersVal:", isUsersVal);
+      let rowIndex = position.split("")[0];
+      let columnIndex = position.split("")[1];
+      let isValRobotsValue =
+        board[rowIndex][columnIndex] === "o" ? true : false;
+      // console.log("isValRobotsValue:", isValRobotsValue);
+      if (isValRobotsValue) return (count = 0);
+      if (isUsersVal) return count++;
+    });
+    console.log("count position----", count);
+    if (count >= 2) {
+      console.log("---- positions selected:", positions);
+      return positions;
+    }
+  });
+  console.log("----- player winning positions -----", playerWinningPositions);
+  return playerWinningPositions;
+}
+/* Reset or Start New Game Code */
+
 function resetBoard() {
   modifyCellsAttribute(false, "");
   emptyBoard();
   displayPlayersTurn(0);
+  players[0].positionsOccupied = [];
 }
 
 function modifyCellsAttribute(disabled, value = null) {
   cells = [...cells];
   cells.forEach((cell) => {
-    if (!disabled) cell.removeAttribute("disabled");
-    cell.setAttribute("disabled", true);
     if (value !== null) cell.value = "";
+    if (!disabled) return cell.removeAttribute("disabled");
+    cell.setAttribute("disabled", true);
   });
 }
 
