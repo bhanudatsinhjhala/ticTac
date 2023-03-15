f306e4f2a5393cd6e06bee112b4b9400bb1e983a
Bhanudatsinh Jhala <jhalabhanudattsinh4090@gmail.com>
2023-03-15 18:10:18 +0530

    created resetgame file

--- index.js
+++ index.js
@@ -103,25 +103,3 @@ function declareWin(playerTurnValue) {
   heading.innerText = `Hurray ${players[index].name} wins`;
   modifyCellsAttribute(true);
 }
-
-function resetBoard() {
-  modifyCellsAttribute(false, "");
-  emptyBoard();
-  displayPlayersTurn(0);
-}
-
-function newGame() {
-  playersForm.style.display = "block";
-  boardForm.style.display = "none";
-  emptyBoard();
-  modifyCellsAttribute(false, "");
-  resetPlayersName();
-}
-function modifyCellsAttribute(disabled, value = null) {
-  cells = [...cells];
-  cells.forEach((cell) => {
-    if (value !== null) cell.value = "";
-    if (!disabled) return cell.removeAttribute("disabled");
-    cell.setAttribute("disabled", true);
-  });
-}
---
+++ resetgame.js
@@ -0,0 +1,21 @@
+function resetBoard() {
+  modifyCellsAttribute(false, "");
+  emptyBoard();
+  displayPlayersTurn(0);
+}
+
+function newGame() {
+  playersForm.style.display = "block";
+  boardForm.style.display = "none";
+  emptyBoard();
+  modifyCellsAttribute(false, "");
+  resetPlayersName();
+}
+function modifyCellsAttribute(disabled, value = null) {
+  cells = [...cells];
+  cells.forEach((cell) => {
+    if (value !== null) cell.value = "";
+    if (!disabled) return cell.removeAttribute("disabled");
+    cell.setAttribute("disabled", true);
+  });
+}
